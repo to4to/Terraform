609fd322b54e2f5cbb021bf3602b3fb514dcceed
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-07-19 22:41:44 +0530

    resources added

---
+++ providers.tf
@@ -0,0 +1,17 @@
+
+
+
+
+provider "aws" {
+  # Configuration options
+  region= "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
